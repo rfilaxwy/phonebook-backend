bb69a18d04ce6db9f02f53798485310061321766
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2020-05-25 12:39:54 -0600

    3.2 complete

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  res.status(200).send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `);
+});
+
 const port = 3001;
 
 app.listen(port, console.log(`Listenting on port ${port}`));
